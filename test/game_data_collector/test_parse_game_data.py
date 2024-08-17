1e319e319e42050eb804c8a3a2b238113d4d5709
thraindk <cedric.mingham@outlook.com>
2024-08-17 23:52:18 +0200

    added shorter match response samples, basic valid test

--- test/game_data_collector/test_parse_game_data.py
+++ test/game_data_collector/test_parse_game_data.py
@@ -9,14 +9,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 
 import game_data_collector
 
-# Mock OpenDota API response
+# load mock OpenDota API response
 
 def api_get_valid_match():
-    with open(os.path.join("resources", "test", "parse_game_data", "valid_match.json"), "r") as fi:
+    with open(os.path.join("resources", "test", "parse_game_data", "match_sample_valid.json"), "r") as fi:
         return json.load(fi)
 
 def api_get_invalid_match():
-    with open(os.path.join("resources", "test", "parse_game_data", "invalid_match.json"), "r") as fi:
+    # invalid match data has region = 2 rest is valid
+    with open(os.path.join("resources", "test", "parse_game_data", "match_sample_invalid.json"), "r") as fi:
         return json.load(fi)
     
 # def api_get_incomplete_match(): # TODO implement
@@ -26,8 +27,25 @@ def api_get_invalid_match():
 
 # Tests
 
+def test_validate_clean_match_data_valid(mocker):
+    # mock get_match_by_id response object
+    mock_response = mocker.MagicMock()
+    mock_response.json.return_value = api_get_valid_match()
+    type(mock_response).status_code = mocker.PropertyMock(return_value=200) # manually add status_code attribute
+
+    # Override 'requests.get' to return the mock response
+    mocker.patch("requests.get", return_value=mock_response)
+    
+    # Test
+    match_res = get_match_by_id(1234) # TODO test method in _utils test class with status 429 etc
+        
+    match = validate_clean_match_data(match_res)
+    assert type(match) is dict
+    # TODO test dropped dict keys from clean match data
+
+
 def test_validate_clean_match_data_invalid(mocker):
-    # mock get_match_by_id response
+    # mock get_match_by_id response object
     mock_response = mocker.MagicMock()
     mock_response.json.return_value = api_get_invalid_match()
     type(mock_response).status_code = mocker.PropertyMock(return_value=200) # manually add status_code attribute
@@ -40,5 +58,5 @@ def test_validate_clean_match_data_invalid(mocker):
     
     assert type(match_res) is dict
     
-    with pytest.raises(ValueError): # TODO test individual match validation criterias
+    with pytest.raises(ValueError): # TODO test individual match validation criterias, test missing key
         validate_clean_match_data(match_res)
