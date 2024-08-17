dff2480f2e7c14667dbf5f8c3846308ac84138d2
thraindk <cedric.mingham@outlook.com>
2024-08-18 00:47:23 +0200

    add tests for make_request_with_retries

---
+++ test/game_data_collector/test_parse_game_data_utils.py
@@ -0,0 +1,47 @@
+import sys
+import os
+import pytest
+import random
+
+import requests
+from game_data_collector.parse_game_data_utils import make_request_with_retries
+
+# Add the src directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
+
+def test_make_request_with_retries_ok(mocker):
+    # mock get_match_by_id response object
+    mock_response = mocker.MagicMock(spec=requests.Response)
+    type(mock_response).status_code = mocker.PropertyMock(return_value=200)
+
+    # Override 'requests.get' to return the mock response
+    mocker.patch("requests.get", return_value=mock_response, max_retries=1)
+    
+    res = make_request_with_retries("http://foo.com")
+    assert isinstance(mock_response, requests.Response)
+    
+def test_make_request_with_retries_force_errors(mocker):
+    # mock get_match_by_id response object
+    mock_response = mocker.MagicMock()
+    type(mock_response).status_code = mocker.PropertyMock(return_value=random.choice([500, 502, 503, 504, 408, 409]))
+
+    # Override 'requests.get' to return the mock response
+    mocker.patch("requests.get", return_value=mock_response)
+    
+    with pytest.raises(ConnectionError): 
+        res = make_request_with_retries("http://foo.com", max_retries=1)
+
+def test_make_request_with_retries_timeout(mocker):
+    # mock get_match_by_id response object
+    mock_response = mocker.MagicMock()
+    type(mock_response).status_code = mocker.PropertyMock(return_value=429)
+
+    # Override 'requests.get' to return the mock response
+    mocker.patch("requests.get", return_value=mock_response)
+    
+    with pytest.raises(ConnectionError): 
+        res = make_request_with_retries("http://foo.com", max_retries=1)
+    
+    
+    
